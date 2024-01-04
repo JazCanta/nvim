66de644dfb454bb774a4909e7de6fad979e4b700
jaz <jperez@cantarellenergy.com>
2024-01-03 19:09:44 -0600

    nvim

---
+++ lua/jazwork/plugins/toggleterm.lua
@@ -0,0 +1,13 @@
+require("toggleterm").setup({
+	float_opts = {
+		border = "single",
+		width = 100,
+		height = 40,
+		winblend = 5,
+		highlights = {
+			border = "Normal",
+			background = "Black",
+		},
+	},
+	open_mapping = [[<Leader>t]],
+})
